bd7d21d52915c22b3b675025d0baa30c865333e8
xin.li <xin.li@quncaotech.com>
2022-02-02 21:23:03 +0800

    feat:添加打包上传

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:lixinks257/docs/<REPO>.git master:gh-pages
+git push -f git@github.com:lixinks257/docs.git master:gh-pages
 
 cd -
